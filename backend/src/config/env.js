20336fd46dc7950d83627eef26060d13e942ae8f
Pranav <pranavadithyarao@gmail.com>
2026-04-12 00:15:42 +0530

    connect db and finish.env

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -1,7 +1,12 @@
 import dotenv from "dotenv";
+dotenv.config();
 
 export const ENV = {
   PORT: process.env.PORT || 5001,
-  MONGO_URI: process.env.MONGO_URI,
+  MONGO_URL: process.env.MONGO_URL,
   NODE_ENV: process.env.NODE_ENV,
+  CLERK_PUBLISHABLE_KEY:process.env.CLERK_PUBLISHABLE_KEY,
+  CLERK_SECRET_KEY:process.env.CLERK_SECRET_KEY,
+  STREAM_API_KEY: process.env.STREAM_API_KEY,
+  STREAM_SECRET:process.env.STREAM_SECRET
 };
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
-import dotenv from "dotenv"
-
+import dotenv from "dotenv";
+import {ENV} from "./config/env.js";
+import {connectDB} from "./config/db.js";
 const app=express();
 
 const PORT= process.env.PORT || 5001;
@@ -9,4 +10,7 @@ app.get("/",(req,res)=>{
     res.send("Hello World");
 });
 
-app.listen(PORT, ()=>console.log("Server started :", PORT));
+app.listen(ENV.PORT, ()=>{
+    console.log("Server started :", ENV.PORT)
+    connectDB();    
+});
