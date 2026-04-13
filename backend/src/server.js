cb12fd02ae22f9811341a93b66dfff2eb1ec534e
Pranav <pranavadithyarao@gmail.com>
2026-04-13 05:44:27 +0530

    fix middleware

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,17 +3,30 @@ import { serve } from "inngest/express";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 import { inngest, functions } from "./config/inngest.js";
+import { clerkMiddleware } from "@clerk/express";
 
 const app = express();
 
 app.use(express.json());
 app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use(clerkMiddleware()); 
 
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
-app.listen(ENV.PORT, () => {
-    console.log("Server started :", ENV.PORT);
-    connectDB();
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+    if (ENV.NODE_ENV !== "production") {
+      app.listen(ENV.PORT, () => {
+        console.log("Server started on port:", ENV.PORT);
+      });
+    }
+  } catch (error) {
+    console.error("Error starting server:", error);
+    process.exit(1); // Exit the process with a failure code
+  }
+};
+
+startServer();
