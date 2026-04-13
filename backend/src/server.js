359d91d7b8ccbefb63ac5f70fa363fbb327be2ec
Pranav <pranavadithyarao@gmail.com>
2026-04-13 05:49:29 +0530

    minor fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,12 +8,15 @@ import { clerkMiddleware } from "@clerk/express";
 const app = express();
 
 app.use(express.json());
-app.use("/api/inngest", serve({ client: inngest, functions }));
+
 app.use(clerkMiddleware()); 
+
 
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 const startServer = async () => {
   try {
